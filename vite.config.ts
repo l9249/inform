ccd81aa48f06f4bf0582654b16912a2b4e6f94c9
linda.dev <>
2026-01-30 22:37:08 +0800

    chore: add gh-pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: process.env.GITHUB_ACTIONS ? '/inform/' : '/',
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
